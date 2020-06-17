833955c82ccf45015d40baab813dbbfb82275ece
Craig Allen <craig@d8atech.com>
2020-06-16 22:07:34 -0500

    Chapter 2 Completed

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Craig'}
-    return '''
-<html>
-    <head>
-        <title>Home Page Microblog</title>
-    </head>
-    <body>    
-        <h1>Hello ''' + user['username'] + '''! Would you like to play a game?</h1>
-    </body>
-</html>'''
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Florence!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
